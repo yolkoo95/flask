62bcb9fa98e3f4fec91c8cdf1c716c9bb9c4a419
unicorn <unicorn@github.com>
2020-03-12 15:50:53 +0800

    add hello0

---
+++ hello/hello0/hello0.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def greet():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
+
