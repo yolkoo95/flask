b554b364bc45cfd07008857e01aae9b388db8562
schronding's cat <nicolaspotato6174@gmail.com>
2020-03-29 16:04:26 +0800

    Added animation

---
+++ xml/js-template/README.md
@@ -0,0 +1,2 @@
+dice - handlebar features, which is applied into posts2
+animation - css animation features, which is applied into posts3&4 
---
+++ xml/js-template/posts/posts2/static/lib.js
@@ -0,0 +1,46 @@
+// start with first post and load 20 posts at a time
+let counter = 1;
+const quantity = 20;
+
+// when to load page
+document.addEventListener('DOMContentLoaded', load);
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        load();
+    }
+}
+
+function load() {
+    // set up start and end numbers, and update counter
+    const start = counter;
+    const end = start + quantity - 1;
+    counter = end + 1; 
+    
+    // get new posts
+    const request = new XMLHttpRequest();
+    request.open('POST', '/posts')
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        data.forEach(add_post);
+    };
+
+    const data = new FormData();
+    data.append('start', start);
+    data.append('end', end);
+
+    request.send(data);
+}
+
+// add new post to html page with handlebar
+const post_template  = Handlebars.compile(document.querySelector('#post-template').innerHTML);
+function add_post(contents) {
+
+    const post = post_template({'contents': contents});
+    document.querySelector('#posts').innerHTML += post;
+
+    document.querySelectorAll('.hide').forEach(button => {
+        button.onclick = function() {
+            this.parentElement.remove();
+        }
+    })
+}
